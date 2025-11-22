2f2c46bed90b6b8184e1f264fd85d81ff8e9f4f2
Georgy <gp.workinbox@gmail.com>
2025-11-22 14:02:53 +0600

    1

---
+++ script.js
@@ -0,0 +1,37 @@
+document.getElementById("rebusForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const answer = document.getElementById("answer").value;
+
+    const correct = "КОНСТИТУЦИЯ";
+
+    const resultDiv = document.getElementById("result");
+    resultDiv.style.display = "block";
+
+    if (answer.trim().toUpperCase() === correct) {
+        resultDiv.innerHTML = `<b>Правильно!</b><br>Ответ: ${correct}`;
+    } else {
+        resultDiv.innerHTML = `<b>Неправильно.</b><br>Ваш ответ: ${answer}<br>Правильный ответ: ${correct}`;
+    }
+
+    // Отправляем данные в GitHub Actions
+    await fetch(
+        "https://api.github.com/repos/folin12/rebus/actions/workflows/send-email.yml/dispatches",
+        {
+            method: "POST",
+            headers: {
+                "Accept": "application/vnd.github+json",
+                "Authorization": "Bearer " + GH_TOKEN,
+            },
+            body: JSON.stringify({
+                ref: "main",
+                inputs: {
+                    name: name,
+                    answer: answer,
+                    correct: correct
+                }
+            })
+        }
+    );
+});
